1df25160cc81413adaf85be3a449c490ceab72aa
Erik Bayerlein <erik.bayer@icloud.com>
2024-08-15 08:27:08 -0300

    adding image class

--- game/background.py
+++ game/background.py
@@ -1,5 +1,5 @@
 from numpy import random
-from cg.cg import Draw
+from cg.cg import Draw, Screen, Texture, TexturePolygon
 
 
 class Star:
@@ -26,8 +26,33 @@ class Background:
         self.windows = windows
         self.viewports = viewports
 
+
     def draw(self, screen) -> None:
         for star in self.stars.pos:
             for x, y in star:
                 Draw.set_pixel(screen, x, y, self.stars.color)
 
+
+class Image:
+    def __init__(self, windows, viewports, nome_img) -> None:
+        self.texture = Texture.import_texture(nome_img)
+        # Obtendo a largura e altura da tela a partir do viewport
+        screen_width = viewports[0][2]
+        screen_height = viewports[0][3]
+
+        # Definindo o TexturePolygon para preencher a tela
+        self.polygon = TexturePolygon(
+            [
+                [0, 0, 0, 0],          # Canto superior esquerdo
+                [0, screen_height, 0, 1],  # Canto inferior esquerdo
+                [screen_width, screen_height, 1, 1], # Canto inferior direito
+                [screen_width, 0, 1, 0]   # Canto superior direito
+            ]
+        )
+        self.windows = windows
+        self.viewports = viewports
+
+
+    def draw(self, screen) -> None:
+        pol = Screen.mapping_window(self.polygon, self.windows[0], self.viewports[0])
+        Texture.scanline_with_texture(screen, pol, self.texture)
